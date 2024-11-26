ce462ede99fc1c8cbc57563f4e6bf76c41c93835
Maulana <maulana.120100@gmail.com>
2024-11-26 20:30:28 +0700

    done

---
+++ src/routes.js
@@ -0,0 +1,29 @@
+const { addBookHandler, getAllBooksHandler, getBookWithIdHandler, editBookHandler, deleteBookHandler } = require('./handler');
+const routes = [
+  {
+    method: 'POST',
+    path: '/books',
+    handler: addBookHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books',
+    handler: getAllBooksHandler,
+  },
+  {
+    method: 'GET',
+    path: '/books/{bookId}',
+    handler: getBookWithIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/books/{bookId}',
+    handler: editBookHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/books/{bookId}',
+    handler: deleteBookHandler,
+  },
+];
+module.exports = routes;
